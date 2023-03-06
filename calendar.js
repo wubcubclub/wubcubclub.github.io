addcaa02e972b9446bca92731611efbd2537cb29
jackal <v@njackal.com>
2023-03-05 21:24:28 -0600

    days of past month and next month dates added to calendar, also each day has a corresponding id

--- calendar.js
+++ calendar.js
@@ -1,5 +1,7 @@
 const dayTemplate = document.getElementById("day-template").content
 const calendar = document.getElementById("calendar");
+const now = new Date();
+const numCells = 6*7;
 let populateCalendar = () => {
     generateDays(calendar,3,31)
 }
@@ -12,16 +14,42 @@ let generateDays = (calendar, offset, days) => {
     for (let i = offset; i < days + offset; i++) {
         currDays.push(i)
     }
+    let dayIds = getMonth(now.getMonth())
 
-    for (let i = 0; i < 6*7; i++) {
+    for (let i = 0; i < numCells; i++) {
         row = Math.floor(i/7)+1;
         col = i%7 +1;
         day = dayTemplate.cloneNode(true);
         day.querySelector("div").className += ` row${row} col${col} ${row===6?"row-last":""} ${!currDays.includes(i)?"not-curr":""}`
-        if(currDays.includes(i)){
-            day.querySelector("a").textContent = i - offset + 1;
-        }
+        day.querySelector("a").textContent = dayIds[i].split("-")[2]
+        day.querySelector("div").id = dayIds[i];
 
         calendar.appendChild(day);
     }
+}
+
+let getMonth = (month) => {
+    let dayIds = []
+    let date = new Date(now.getFullYear(),month)
+    let lastMonth = new Date(now.getFullYear(),month,0)
+    let lastMonthEnd = lastMonth.getDate()
+    let monthEnd = new Date(now.getFullYear(),month+1,0).getDate()
+    let weekday = date.getDay()
+
+    let lastMonthYear = lastMonth.getFullYear();
+    for (let i = weekday-1; i >= 0; i--) {
+        dayIds.push(`${lastMonthYear}-${month-1}-${lastMonthEnd-i}`)
+    }
+    let year = now.getFullYear();
+    for (let i = weekday; i < monthEnd + weekday; i++) {
+        dayIds.push(`${year}-${month}-${i - weekday+1}`)
+    }
+    let nextMonth = new Date(now.getFullYear(),month,monthEnd+1)
+    let nextMonthYear = nextMonth.getFullYear()
+    let nextMonthMonth = nextMonth.getMonth()
+    let t = 1;
+    for (let i = weekday + monthEnd; i < numCells; i++) {
+        dayIds.push(`${nextMonthYear}-${nextMonthMonth}-${t++}`)
+    }
+    return dayIds
 }
